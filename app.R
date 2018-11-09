4c51afbc436e47ca54e900a221e80d2362d06fb7
Grej Segura <mr_grej@yahoo.com>
2018-11-09 13:20:44 +0400

    Update app.R

--- app.R
+++ app.R
@@ -48,7 +48,7 @@ ui <- shinyUI(
 
 server <- shinyServer(function(input, output){
         
-                predict <- reactiveValues({
+                predictTable <- reactiveValues({
                         sentence <- input$searchBar
                         wordBreakDown <- unlist(replaceText(sentence))
                         wordBreakDown <- strsplit(wordBreakDown, split = " ")[[1]]
@@ -75,9 +75,14 @@ server <- shinyServer(function(input, output){
                                         if (nrow(scoreTable) < 5){
                                                 addUniGram <- as.data.frame(unigramData[1:(5-nrow(scoreTable)), c('nextWord')])
                                                 scoreTable <- as.data.frame(full_join(scoreTable, addUniGram, by = 'nextWord'))
+                                                return(scoreTable)
                                         }
+                                        return(scoreTable)
                                 }
                         }
+                })
+                output$predict <- renderText({
+                        return(as.character(unlist(predictTable())))
                 })
         
 })                        

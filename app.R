cfad214b12ad580fff118c1a29c347086fd87cbf
Grej Segura <mr_grej@yahoo.com>
2018-11-10 00:21:39 +0400

    Update app.R

--- app.R
+++ app.R
@@ -21,9 +21,12 @@ ui <- shinyUI(
                                         h4(" ", style="padding:20px;"))),
                                 fluidRow(column(12, align="center",
                                         div(style = "font-size:16px; color:#707B7C; padding:20px;","Top predicted next word:"))),
-                                fluidRow(column(12, align="center",
-                                        actionButton(style="font-weight:normal; color: #202121; background-color: #D7DDE3; font-size: 16px; text-transform: lowercase","button",
-                                                textOutput('predict'))))
+                                fluidRow(column(6, align="center",
+                                        actionButton(style="font-weight:normal; color: #202121; background-color: #D7DDE3; font-size: 16px; text-transform: lowercase","button1",
+                                                textOutput('predict1'))),
+                                        column(10, align="center",
+                                        actionButton(style="font-weight:normal; color: #202121; background-color: #D7DDE3; font-size: 16px; text-transform: lowercase","button1",
+                                                textOutput('predict2'))))
                                 #fluidRow(column(12, align="center",
                                 #        div(style = "font-weight:bold; font-size:22px; color:#2471A3; font-family:courier new", 
                                 #            textOutput("predict"))))
@@ -48,7 +51,7 @@ ui <- shinyUI(
 
 server <- shinyServer(function(input, output){
         
-                predictTable <- reactive({
+                predictTable1 <- reactive({
                         sentence <- input$searchBar
                         wordBreakDown <- unlist(replaceText(sentence))
                         wordBreakDown <- strsplit(wordBreakDown, split = " ")[[1]]
@@ -75,16 +78,55 @@ server <- shinyServer(function(input, output){
                                         if (nrow(scoreTable) < 5){
                                                 addUniGram <- as.data.frame(unigramData[1:(5-nrow(scoreTable)), c('nextWord')])
                                                 scoreTable <- as.data.frame(full_join(scoreTable, addUniGram, by = 'nextWord'))
-                                                return(scoreTable[1:5, c('nextWord')])
+                                                return(scoreTable[1, c('nextWord')])
                                         }
-                                        return(scoreTable[1:5, c('nextWord')])
+                                        return(scoreTable[1, c('nextWord')])
                                 }
                         }
                 })
                 observeEvent(input$button1,
-                        {output$predict <- renderText({
-                                return(as.character(unlist(predictTable())))
+                        {output$predict1 <- renderText({
+                                return(as.character(unlist(predictTable1())))
                         })
+                })
+#################################################################################################                
+                predictTable2 <- reactive({
+                        sentence <- input$searchBar
+                        wordBreakDown <- unlist(replaceText(sentence))
+                        wordBreakDown <- strsplit(wordBreakDown, split = " ")[[1]]
+                        numberWords <- length(wordBreakDown)
+                        scoreTable <- data.frame()
+                        if(numberWords == 0){
+                                scoreTable <- as.character('?????')
+                        } else if(numberWords >= 1){
+                                # match 4 words with nGramData
+                                if (numberWords > 4){
+                                        numberWords = 4
+                                }
+                                scoreTable <- data.frame()
+                                for (i in 1:numberWords){
+                                        lastWords <- wordBreakDown[(length(wordBreakDown)-i):length(wordBreakDown)]
+                                        lastWords <- paste(lastWords, collapse = " ")
+                                        setOfNextWord <- nGramData[nGramData$typedWord %in% lastWords, ]
+                                        countWords <- setOfNextWord %>% group_by(nextWord) %>% 
+                                                summarise(frequency = n()) %>% arrange(desc(frequency)) %>% top_n(n = 5, wt = frequency) %>%
+                                                mutate(score = ((0.4^((numberWords+1)-i))*frequency)/nrow(setOfNextWord))
+                                        scoreTable <- as.data.frame(rbind(scoreTable, countWords))
+                                        scoreTable <- scoreTable %>% top_n(n = 5, wt = score)
+                                        scoreTable <- scoreTable[!(is.na(scoreTable$nextWord)),]
+                                        if (nrow(scoreTable) < 5){
+                                                addUniGram <- as.data.frame(unigramData[1:(5-nrow(scoreTable)), c('nextWord')])
+                                                scoreTable <- as.data.frame(full_join(scoreTable, addUniGram, by = 'nextWord'))
+                                                return(scoreTable[2, c('nextWord')])
+                                        }
+                                        return(scoreTable[2, c('nextWord')])
+                                }
+                        }
+                })
+                observeEvent(input$button2,
+                             {output$predict2 <- renderText({
+                                     return(as.character(unlist(predictTable2())))
+                             })
                 })
 })                        
 
